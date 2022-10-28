b5c892d6ffc8dde8430bcb47da7b348bc386704c
Kagweitis <kagweitis@gmail.com>
2022-10-28 16:05:48 +0300

    new undertakings

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,13 +3,42 @@
 </script>
 
 <template>
-  <img src="../assets/img/ncba card.png" alt="card image">
+  <!-- <img src="../assets/img/ncba card.png" alt="card image"> -->
+
+  <div class="card">
+
+      <img class="logo" src="../assets/img/ncba logo.png" alt="logo">
+      <p>
+        CARD HOLDER NAME
+      </p>
+
+      <img class="chip" src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" alt="chip">
+
+   
+
+
+  </div>
 </template>
 
 <style scope>
-  img{
-    /* filter: drop-shadow(5px 5px 5px grey); */
+  /* img{
     box-shadow: 0 0 40px darkgray;
       
+  } */
+
+  .card{
+    width: 80%;
+    /* height: 10%; */
+    background: linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%);
+    margin-bottom: 30px;
+    box-shadow: 0 0 40px lightblue;
+    border-radius: 20px;
   }
+  img{
+    display: flex;
+    flex-direction: column;
+    size: 10%;
+    /* padding: 10px; */
+  }
+ 
 </style>
